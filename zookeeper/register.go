04682fe2d5b3fe4cc81b347fa6367ea2e1261b39
laonsx <laonsx@163.com>
2019-08-23 18:21:44 +0800

    zk-grpc

--- zookeeper/register.go
+++ zookeeper/register.go
@@ -23,7 +23,22 @@ func Register(target, server, value string) error {
 	path := "/" + schema + "/" + server + "/" + value
 
 	_, err = zkc.Create(path, nil, 0, zk.WorldACL(zk.PermAll))
-	if err != nil {
+	switch {
+	case err == zk.ErrNoNode:
+
+		_, _ = zkc.Create("/"+schema, nil, 0, zk.WorldACL(zk.PermAll))
+		_, _ = zkc.Create("/"+schema+"/"+server, nil, 0, zk.WorldACL(zk.PermAll))
+		_, err := zkc.Create(path, nil, 0, zk.WorldACL(zk.PermAll))
+		if err != nil {
+
+			return err
+		}
+
+	case err == zk.ErrNodeExists:
+
+	case err == nil:
+
+	default:
 
 		return err
 	}
